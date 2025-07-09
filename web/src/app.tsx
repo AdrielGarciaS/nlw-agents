87376b0b34a65792a41f378edcd6f2a9f37c6875
Adriel Garcia <adrielgarcia@live.com>
2025-07-09 20:42:58 -0300

    feat: setup react-router-dom and react-query

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,12 +1,20 @@
-import { Wand } from 'lucide-react';
-import { Button } from './components/ui/button';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CreateRoom } from './pages/create-room';
+import { Room } from './pages/room';
+
+const queryClient = new QueryClient();
 
 export function App() {
   return (
-    <div>
-      <Button size="icon" variant="outline">
-        <Wand />
-      </Button>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
